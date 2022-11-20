1615d1c92308ca9840b959dd27b6f1104beb9eb3
kmanka1 <77521313+kmanka1@users.noreply.github.com>
2022-11-19 22:42:48 -0500

    Changes to time buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import Search from './components/search';
+import ShowTime from './components/showTime'
 import './App.css';
 import mapitLogo from './assets/Logo.png'
 import mapImage from './assets/ACEBmap.png'
@@ -18,21 +19,8 @@ function App() {
         <img src = {mapImage} width='800px' height='280px'></img>
         <br></br>
 
-        <div class='times'>
-          <a href='#at8am'>8:00 AM</a>
-          <a href='#at9am'>9:00 AM</a>
-          <a href='#at10am'>10:00 AM</a>
-          <a href='#at11am'>11:00 AM</a>
-          <a href='#at12pm'>12:00 PM</a>
-          <a href='#at1pm'>1:00 PM</a>
-          <a href='#at2pm'>2:00 PM</a>
-          <a href='#at3pm'>3:00 PM</a>
-          <a href='#at4pm'>4:00 PM</a>
-          <a href='#at5pm'>5:00 PM</a>
-          <a href='#at6pm'>6:00 PM</a>
-          <a href='#at7pm'>7:00 PM</a>
-          <a href='#at8pm'>8:00 PM</a>
-        </div>
+        <ShowTime/>
+
       </header>
     </div>
   );
